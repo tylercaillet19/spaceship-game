1d7971038e8a85d04d5dcf7ddc3c0713f6cd22f5
tylercaillet19 <118496393+tylercaillet19@users.noreply.github.com>
2022-11-22 22:58:37 -0600

    update

--- game_with_tutorial.py
+++ game_with_tutorial.py
@@ -9,8 +9,9 @@ BG_HEIGHT, BG_WIDTH = 1500, 800
 BG_IMAGE = pg.image.load(os.path.join('Assets', '5379387.webp'))
 BG = pg.transform.scale(BG_IMAGE, (BG_HEIGHT, BG_WIDTH))
 
+BORDER = pg.Rect(WIDTH/2 - 5, 0, 10, HEIGHT)
 
-FPS = 60
+FPS = 144
 VEL = 10
 BOOST_VEL = 15
 
@@ -30,6 +31,7 @@ BULLET = pg.transform.scale(BULLET_IMAGE, (BULLET_WIDTH, BULLET_HEIGHT))
 
 def draw_window(yellow, bullet):
     WIN.blit(BG, (0, 0))
+    pg.draw.rect(WIN, BACKGROUND, BORDER)
     WIN.blit(SPACESHIP, (yellow.x, yellow.y))
     WIN.blit(BULLET, (bullet.x, bullet.y))
     pg.display.update()
@@ -37,21 +39,21 @@ def draw_window(yellow, bullet):
 
 def spaceship_movement(keys_pressed, yellow):
     global SPACESHIP, ROTATION
-    if keys_pressed[pg.K_a]:  # LEFT
+    if keys_pressed[pg.K_a] and yellow.x - VEL > 0:  # LEFT
         yellow.x -= VEL
-    if keys_pressed[pg.K_d]:
+    if keys_pressed[pg.K_d] and yellow.x + VEL + yellow.width < 1500:
         yellow.x += VEL
-    if keys_pressed[pg.K_w]:
+    if keys_pressed[pg.K_w] and yellow.y - VEL > 0:
         yellow.y -= VEL
-    if keys_pressed[pg.K_s]:
+    if keys_pressed[pg.K_s] and yellow.y + VEL + yellow.height < 800:
         yellow.y += VEL
-    if keys_pressed[pg.K_LSHIFT] and keys_pressed[pg.K_a]:
+    if keys_pressed[pg.K_LSHIFT] and keys_pressed[pg.K_a] and yellow.x - VEL > 0:
         yellow.x -= BOOST_VEL
-    if keys_pressed[pg.K_LSHIFT] and keys_pressed[pg.K_d]:
+    if keys_pressed[pg.K_LSHIFT] and keys_pressed[pg.K_d] and yellow.x + VEL + yellow.width < 1500:
         yellow.x += BOOST_VEL
-    if keys_pressed[pg.K_LSHIFT] and keys_pressed[pg.K_s]:
+    if keys_pressed[pg.K_LSHIFT] and keys_pressed[pg.K_s] and yellow.y + VEL + yellow.height < 800:
         yellow.y += BOOST_VEL
-    if keys_pressed[pg.K_LSHIFT] and keys_pressed[pg.K_w]:
+    if keys_pressed[pg.K_LSHIFT] and keys_pressed[pg.K_w] and yellow.y - VEL > 0:
         yellow.y -= BOOST_VEL
 
 
